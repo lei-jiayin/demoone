62752247501b2a81e9fd6598819f7779e74a7bb3
xw <3597906135@qq.com>
2020-05-19 18:04:28 +0800

    16

---
+++ src/main/java/com/demo2020/cods/leetCode/offer/Solution16.java
@@ -0,0 +1,29 @@
+package com.demo2020.cods.leetCode.offer;
+
+/**
+ * 数值的整数次方
+ * @author xw
+ * @date 2020/5/19 11:03
+ */
+public class Solution16 {
+    public double myPow(double x, int n) {
+        if(x == 0) return 0;
+        long b = n;
+        double res = 1.0;
+        if(b < 0) {
+            x = 1 / x;
+            b = -b;
+        }
+        while(b > 0) {
+            if((b & 1) == 1) res *= x;
+            x *= x;
+            b >>= 1;
+        }
+        return res;
+    }
+
+    //作者：jyd
+    //链接：https://leetcode-cn.com/problems/shu-zhi-de-zheng-shu-ci-fang-lcof/solution/mian-shi-ti-16-shu-zhi-de-zheng-shu-ci-fang-kuai-s/
+    //来源：力扣（LeetCode）
+    //著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
